02ef24090ad7904417aed2566e779de13866fda6
dextervargasDev <dextervargas0912@gmail.com>
2025-10-22 09:44:18 +0800

    learning collections

--- src/main/java/com/dexterv/learn/collections/CollectionsPlayground.java
+++ src/main/java/com/dexterv/learn/collections/CollectionsPlayground.java
@@ -4,7 +4,10 @@ import com.dexterv.domain.Employee;
 import com.dexterv.repository.EmployeeData;
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CollectionsPlayground {
@@ -17,13 +20,65 @@ public class CollectionsPlayground {
     }
 
     public void employeeData(){
-        // filter active players
+        // 1️⃣ Filter active employees
         List<Employee> activeEmployees = employees.stream()
                 .filter(Employee::isActive)
                 .collect(Collectors.toList()); // can be replace with toList();
-
+        System.out.println("\nGet Active Employees:");
         for (Employee employee : activeEmployees) {
             System.out.println(employee);
+        }
+
+        // 2️⃣ Sort employees by salary descending
+        List<Employee> sortedBySalary = employees.stream()
+                .sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
+                .toList();
+
+        System.out.println("\nSort By Salary:");
+        for (Employee employee : sortedBySalary) {
+            System.out.println(employee);
+        }
+
+        // 3️⃣ Group by department
+        Map<String, List<Employee>> groupByDepartment = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+
+        System.out.println("\n Map<String, List<Employee>> | Grouped By Department:");
+        for (Map.Entry<String, List<Employee>> items : groupByDepartment.entrySet()) {
+
+            String department = items.getKey();
+            List<Employee> employees = items.getValue();
+
+            System.out.println("\t" + department + ":");
+            for (Employee employee : employees) {
+                System.out.println("\t\t" + employee);
+            }
+
+        }
+
+        // 4️⃣ Extract all unique skills
+        Set<String> allSkills = employees.stream()
+                .flatMap(e -> e.getSkills().stream())
+                .collect(Collectors.toSet());
+
+        System.out.println("\nSet<String> | flatMap | All Skills:");
+        for (String skill : allSkills) {
+            System.out.println(skill);
+        }
+
+        // 5️⃣ Average salary per department
+        Map<String, Double> avgSalaryByDepartment = employees.stream()
+                .collect(
+                        Collectors.groupingBy(Employee::getDepartment,
+                                Collectors.averagingDouble(Employee::getSalary)
+                        ));
+
+        System.out.println("\n Map<String, Double> | Get Average Salary By Department");
+        for (Map.Entry<String, Double> items : avgSalaryByDepartment.entrySet()) {
+            String department = items.getKey();
+            Double salary = avgSalaryByDepartment.get(department);
+
+            System.out.println("\t" + department + ": " + salary);
         }
     }
 }
